37dfd1ddedabead36c0dfaba4e0dbf5f487952bb
Kamil Kijak <kamil.kijak@pixlesofte.com>
2025-05-29 21:44:28 +0200

    created section about me

--- src/Components/Pages/AboutMe.jsx
+++ src/Components/Pages/AboutMe.jsx
@@ -1,8 +1,81 @@
 
+import PolandFlag from "../../img/aboutMe/polandFlag.jpg"
+import Programming from "../../img/aboutMe/programming.jpg"
+import Hobbies from "../../img/aboutMe/hobbies.jpg"
+import Success from "../../img/aboutMe/success.jpg"
+import Mind from "../../img/aboutMe/mind.jpg"
+
 export default function AboutMe() {
     return (
-        <main className="bg-zinc-900 min-h-screen">
-            
+        <main className="bg-zinc-900 min-h-screen pt-20 px-15">
+            <section className="mt-40 pt-5 mx-8 shadow-xl shadow-red-700 rounded-2xl">
+                <section className="flex flex-row md:justify-start justify-center">
+                    <span className="text-6xl h-[5rem] md:ml-8 ml-0 self-center text-transparent bg-gradient-to-r from-red-700 to-orange-700 bg-clip-text font-extrabold ">My origin</span>
+                </section>
+                <section className="mt-10 p-5 border-t-4 border-red-700
+                 rounded-2xl flex flex-col md:flex-row justify-between items-center">
+                    <span className="text-white font-bold text-xl basis-3/4 mr-0 md:mr-10 text-center md:text-left text-shadow-red-900 text-shadow-lg">
+                        I am from Poland. Also my home language is Polish. I live near a Cracow city in small town, but I want to make carrier in Cracow city.
+                         I have good plans about it.
+                    </span>
+                    <img src={PolandFlag} className="rounded-2xl basis-1/4 h-[200px] w-[400px] md:mt-0 mt-10"/>
+                </section>
+            </section>
+            <section className="mt-40 pt-5 mx-8 shadow-xl shadow-purple-500 rounded-2xl">
+                <section className="flex flex-row md:justify-start justify-center">
+                    <span className="text-6xl h-[5rem] md:ml-8 ml-0 self-center text-transparent bg-gradient-to-r from-purple-500 to-fuchsia-700 bg-clip-text font-extrabold ">Character</span>
+                </section>
+                <section className="mt-10 p-5 border-t-4 border-purple-500
+                 rounded-2xl flex flex-col md:flex-row justify-between items-center">
+                    <span className="text-white font-bold text-xl basis-3/4 mr-0 md:mr-10 text-center md:text-left text-shadow-fuchsia-900 text-shadow-lg">
+                        I always prefer cooperation to solve problems. I am very productive and
+                         I like to starting a something new with projects. I can also find a good solution of the problem.
+                         Also i can be a solo-worker i like work on my own with problems or project.
+                         I have culture and i also can understand other people.
+                    </span>
+                    <img src={Mind} className="rounded-2xl basis-1/4 h-[200px] w-[400px] md:mt-0 mt-10"/>
+                </section>
+            </section>
+            <section className="mt-40 pt-5 mx-8 shadow-xl shadow-cyan-500 rounded-2xl">
+                <section className="flex flex-row md:justify-start justify-center">
+                    <span className="text-6xl h-[5rem] md:ml-8 ml-0 self-center text-transparent bg-gradient-to-r from-cyan-500 to-blue-700 bg-clip-text font-extrabold ">Why IT?</span>
+                </section>
+                <section className="mt-10 p-5 border-t-4 border-cyan-500
+                 rounded-2xl flex flex-col md:flex-row justify-between items-center">
+                    <span className="text-white font-bold text-xl basis-3/4 mr-0 md:mr-10 text-center md:text-left text-shadow-blue-900 text-shadow-lg">
+                        Tech things is always my main passion because i love logical things as Math and problem solving, also i know about computers and i was interested about how computers work. So i choose IT brand because I like it.
+                    </span>
+                    <img src={Programming} className="rounded-2xl basis-1/4 h-[200px] w-[400px] md:mt-0 mt-10"/>
+                </section>
+            </section>
+            <section className="mt-40 pt-5 mx-8 shadow-xl shadow-orange-500 rounded-2xl">
+                <section className="flex flex-row md:justify-start justify-center">
+                    <span className="text-6xl h-[5rem] md:ml-8 ml-0 self-center text-transparent bg-gradient-to-r from-orange-700 to-yellow-700 bg-clip-text font-extrabold ">My hobbies</span>
+                </section>
+                <section className="mt-10 p-5 border-t-4 border-orange-500
+                 rounded-2xl flex flex-col md:flex-row justify-between items-center">
+                    <span className="text-white font-bold text-xl basis-3/4 mr-0 md:mr-10 text-center md:text-left text-shadow-orange-900 text-shadow-lg">
+                        I have many different hobbies. One of them is game programming I think that this
+                         is a very creative to create own games, this required large amount of skills and patience, also i like playing games sometimes for relax,
+                          create a pixelart style paintings. Also i like a create new different thinks and works with there.
+                    </span>
+                    <img src={Hobbies} className="rounded-2xl basis-1/4 h-[200px] w-[400px] md:mt-0 mt-10"/>
+                </section>
+            </section>
+            <section className="mt-40 pt-5 mx-8 shadow-xl shadow-yellow-500 rounded-2xl">
+                <section className="flex flex-row md:justify-start justify-center">
+                    <span className="text-6xl h-[5rem] md:ml-8 ml-0 self-center text-transparent bg-gradient-to-r from-yellow-500 to-amber-500 bg-clip-text font-extrabold ">My future</span>
+                </section>
+                <section className="mt-10 p-5 border-t-4 border-yellow-500
+                 rounded-2xl flex flex-col md:flex-row justify-between items-center">
+                    <span className="text-white font-bold text-xl basis-3/4 mr-0 md:mr-10 text-center md:text-left text-shadow-yellow-900 text-shadow-lg">
+                        My target is be more productive and improve my skills, also i want to create something big with this learned skills
+                        I try to be productive to complete all my plans for life and be satisfacted with my complete work and projects.
+                    </span>
+                    <img src={Success} className="rounded-2xl basis-1/4 h-[200px] w-[400px] md:mt-0 mt-10"/>
+                </section>
+            </section>
+            <section className="mt-20 h-20 w-full"> </section>
         </main>
     )
 }
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -9,12 +9,12 @@ export default function Home() {
             <section className="flex flex-col justify-center items-center h-screen bg-gradient-to-t from-zinc-900 to-zinc-950">
                 <FontAwesomeIcon icon={faStar} className="text-blue-700 text-8xl mb-6"/>
                 <span className="md:text-8xl text-6xl font-extrabold bg-gradient-to-r from-blue-700 to-purple-700 bg-clip-text text-transparent">Welcome!</span>
-                <span className="text-5xl font-extrabold text-zinc-600 mt-10">To My site</span>
+                <span className="text-5xl font-extrabold text-zinc-600 mt-10 text-shadow-lg text-shadow-zinc-500">To My site</span>
             </section>
             <section className="flex flex-col justify-center items-center">
                 <section className="flex flex-col items-center mt-20">
                     <FontAwesomeIcon icon={faCircleInfo} className="text-blue-700 text-6xl"/>
-                    <span className="text-white text-6xl font-bold">My Info</span>
+                    <span className="text-cyan-500 text-6xl font-extrabold">My Info</span>
                     <div className="w-full border-t-4 border-t-white mt-4"></div>
                 </section>
                 <section className="sm:mx-50 mx-25 mt-5">
@@ -25,7 +25,7 @@ export default function Home() {
                 </section>
                 <section className="flex flex-col items-center mt-50">
                     <FontAwesomeIcon icon={faBriefcase} className="text-red-700 text-6xl"/>
-                    <span className="text-white text-6xl font-bold">Preferences</span>
+                    <span className="text-red-600 text-6xl font-extrabold">Preferences</span>
                     <div className="w-full border-t-4 border-t-white mt-4"></div>
                 </section>
                 <section className="sm:mx-50 mx-25 mt-5">
@@ -37,10 +37,10 @@ export default function Home() {
                 </section>
                 <section className="flex flex-col items-center mt-50">
                     <FontAwesomeIcon icon={faTrophy} className="text-yellow-500 text-6xl"/>
-                    <span className="text-white text-6xl font-bold">Goal</span>
+                    <span className="text-yellow-400 text-6xl font-extrabold">Goal</span>
                     <div className="w-full border-t-4 border-t-white mt-4"></div>
                 </section>
-                <section className="sm:mx-50 mx-25 my-5 ">
+                <section className="sm:mx-50 mx-25 mt-5 mb-20 ">
                     <span className="text-white text-wrap font-bold text-xl">
                         My goal is extend my skills and improve my work for better work.
                         By builting projects I learn new skills, new technologies and improve work with another projects.
